6f91b38f772e3673fdf1f37237deb9f31de1cc1d
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-10-23 07:31:17 +0100

    Created Button Component

---
+++ frontend/src/components/common/Button.jsx
@@ -0,0 +1,40 @@
+// NODE_MODULES
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  button: {
+    color: "var(--primaryText)",
+    border: "1px solid var(--primaryBorder)",
+    padding: "6px 12px",
+    backgroundColor: "transparent",
+    borderRadius: 4,
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "var(--primaryColor)",
+      color: "var(--white)",
+      borderColor: "var(--primaryColor)",
+    },
+  },
+});
+
+const Button = ({ startIcon, text, endIcon, customStyles }) => {
+  const classes = useStyles();
+
+  return (
+    <button className={classes.button} style={customStyles.button}>
+      {startIcon && (
+        <span className={classes.startIcon} style={customStyles.startIcon}>
+          {startIcon}
+        </span>
+      )}
+      {text}
+      {endIcon && (
+        <span className={classes.endIcon} style={customStyles.endIcon}>
+          {endIcon}
+        </span>
+      )}
+    </button>
+  );
+};
+
+export default Button;
