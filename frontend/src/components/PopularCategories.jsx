48d66b0e511c60131516a35ff3c332cb2965cd2f
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-10-25 13:22:29 +0100

    Completed PopularCategories components

---
+++ frontend/src/components/PopularCategories.jsx
@@ -0,0 +1,101 @@
+// NODE_MODULES
+import { makeStyles } from "@mui/styles";
+
+// LOCAL IMPORTS
+import fruits from "../assets/images/fruits.jpg";
+import vegetables from "../assets/images/vegetables.jpg";
+import spices from "../assets/images/spices.jpg";
+import pulses from "../assets/images/pulses.jpg";
+import cereals from "../assets/images/cereals.jpg";
+import seeds from "../assets/images/seeds.jpg";
+// import fibres from "../assets/images/fibres.jpg";
+import { Container, Grid } from "@mui/material";
+
+const useStyles = makeStyles({
+  image: {
+    border: "1px solid var(--primaryBorder)",
+    borderRadius: "50%",
+    maxWidth: 150,
+    width: "90%",
+  },
+  category: {
+    width: "auto",
+    textDecoration: "none",
+  },
+  popularCatogories: {
+    textAlign: "center",
+    alignItems: "center",
+    margin: "30px 0 !important",
+    overflow: "hidden",
+  },
+  title: {
+    color: "var(--primaryText)",
+    margin: "5px 0 15px 0",
+  },
+});
+
+const PopularCategories = ({ mediaQueries }) => {
+  const { mediumDown } = mediaQueries;
+  const classes = useStyles();
+
+  const mediaStyles = {
+    title: {
+      fontSize: mediumDown && 16,
+    },
+  };
+
+  return (
+    <Container>
+      <Grid container className={classes.popularCatogories}>
+        {categories.map((category, index) => (
+          <Grid item xs={4} md={2}>
+            <a key={index} href={category.link} className={classes.category}>
+              <img className={classes.image} src={category.image} alt="" />
+              <h3 className={classes.title} style={mediaStyles.title}>{category.title}</h3>
+            </a>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+const categories = [
+  {
+    title: "Fruits",
+    image: fruits,
+    link: "fruits",
+  },
+  {
+    title: "Pulses",
+    image: pulses,
+    link: "pulses",
+  },
+  {
+    title: "Vegetables",
+    image: vegetables,
+    link: "vegetables",
+  },
+  {
+    title: "Cereals",
+    image: cereals,
+    link: "cereals",
+  },
+  {
+    title: "Seeds",
+    image: seeds,
+    link: "seeds",
+  },
+  {
+    title: "Spices",
+    image: spices,
+    link: "spices",
+  },
+  // {
+  //   title: "Fibres",
+  //   image: fibres,
+  //   link: "fibres",
+  // },
+];
+
+export default PopularCategories;
--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -2,9 +2,9 @@
 import landing_1 from "../../assets/images/landing1.png";
 import landing_2 from "../../assets/images/landing2.png";
 import Carousel from "../common/Carousel";
+import PopularCategories from "../PopularCategories";
 
-
-const Landing = ({mediaQueries}) => {
+const Landing = ({ mediaQueries }) => {
   const slides = [
     {
       image: landing_1,
@@ -19,6 +19,7 @@ const Landing = ({mediaQueries}) => {
   return (
     <div>
       <Carousel slides={slides} />
+      <PopularCategories mediaQueries={mediaQueries} />
     </div>
   );
 };
