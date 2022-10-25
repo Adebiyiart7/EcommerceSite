d400008e863ea2baa999dcd8e3344bec1c4f37c5
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-10-25 21:28:52 +0100

    Completed Offer component

--- frontend/src/components/common/item2/Index.jsx
+++ frontend/src/components/common/item2/Index.jsx
@@ -1,11 +1,106 @@
-import React from 'react'
+// NODE_MODULES
+import { Link } from "react-router-dom";
+import { makeStyles } from "@mui/styles";
+import { Rating } from "@mui/material";
+import { BiHeart } from "react-icons/bi";
 
-const Item = () => {
+const useStyles = makeStyles({
+  actions: {
+    display: "flex",
+  },
+  image: {
+    width: "100%",
+    maxWidth: 120,
+  },
+  itemContainer: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "row",
+    borderRadius: 7,
+    textDecoration: "none",
+    padding: 16,
+    margin: "auto",
+    height: 120,
+    border: "1px solid var(--white)",
+    backgroundColor: "var(--white)",
+    "&:hover": {
+      border: "1px solid var(--primaryColor)",
+    },
+  },
+  price: {
+    fontWeight: 700,
+    fontSize: 20,
+    margin: "5px 0",
+  },
+  right: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    marginLeft: 10,
+  },
+  stars: {
+    // marginTop: 10,
+  },
+  title: {
+    margin: 0,
+    fontSize: 16,
+  },
+  wishListIcon: {
+    padding: 6,
+    cursor: "pointer",
+    color: "var(--primaryText)",
+    fontSize: 14,
+    marginLeft: 10,
+    border: "1px solid var(--primaryBorder)",
+    "&:hover": {
+      color: "var(--white)",
+      border: "1px solid var(--primaryColor)",
+      backgroundColor: "var(--primaryColor)",
+    },
+  },
+});
+
+const Item = ({ action, image, price, stars, title, mediaQueries }) => {
+  const { laptopUp } = mediaQueries;
+  const classes = useStyles();
+
+  const mediaStyles = {
+    wishListIcon: {
+      fontSize: laptopUp ? 16 : 14,
+    },
+  };
+
   return (
-    <div>
-      
+    <div className={classes.itemContainer}>
+      <div className={classes.left}>
+        <Link to="/">
+          <img className={classes.image} src={image} alt="" />
+        </Link>
+      </div>
+      <div className={classes.right}>
+        <div>
+          <h3 className={classes.title}>{title}</h3>
+          <div className={classes.price}>${price}</div>
+          <Rating
+            value={stars}
+            size="small"
+            readOnly
+            style={{
+              color: "var(--primaryColor)",
+            }}
+            className={classes.stars}
+          />
+        </div>
+        <div className={classes.actions}>
+          <span>{action}</span>
+          <BiHeart
+            style={mediaStyles.wishListIcon}
+            className={classes.wishListIcon}
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
--- frontend/src/components/offer/Index.jsx
+++ frontend/src/components/offer/Index.jsx
@@ -1,8 +1,12 @@
 // NODE_MODULES
+import { Container, Grid } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 // LOCAL IMPORTS
 import PageTitle from "../common/PageTitle";
+import oranges from "../../assets/images/oranges.jpg";
+import Button from "../common/Button";
+import Item from "../common/item2/Index";
 
 const useStyles = makeStyles({
   offer: {
@@ -10,6 +14,20 @@ const useStyles = makeStyles({
     backgroundColor: "var(--lightBackground)",
   },
 });
+
+const ItemButton = ({ mediaQueries }) => {
+  const { laptopUp } = mediaQueries;
+
+  const customStyles = {
+    button: {
+      borderRadius: 0,
+      padding: "8px 16",
+      fontSize: laptopUp ? 14 : 12,
+    },
+  };
+
+  return <Button text="Add to Cart" customStyles={customStyles} />;
+};
 
 const Offer = ({ mediaQueries }) => {
   const classes = useStyles();
@@ -21,6 +39,70 @@ const Offer = ({ mediaQueries }) => {
         subTitle="Hurry and Grab Yours"
         mediaQueries={mediaQueries}
       />
+      <Container>
+        <Grid container spacing={{ xs: 2 }}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Item
+              action={<ItemButton mediaQueries={mediaQueries} />}
+              image={oranges}
+              price={20}
+              stars={4}
+              title="Soure Fresh Lemons"
+              mediaQueries={mediaQueries}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Item
+              action={<ItemButton mediaQueries={mediaQueries} />}
+              image={oranges}
+              price={20}
+              stars={4}
+              title="Soure Fresh Lemons"
+              mediaQueries={mediaQueries}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Item
+              action={<ItemButton mediaQueries={mediaQueries} />}
+              image={oranges}
+              price={20}
+              stars={4}
+              title="Soure Fresh Lemons"
+              mediaQueries={mediaQueries}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Item
+              action={<ItemButton mediaQueries={mediaQueries} />}
+              image={oranges}
+              price={20}
+              stars={4}
+              title="Soure Fresh Lemons"
+              mediaQueries={mediaQueries}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Item
+              action={<ItemButton mediaQueries={mediaQueries} />}
+              image={oranges}
+              price={20}
+              stars={4}
+              title="Soure Fresh Lemons"
+              mediaQueries={mediaQueries}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Item
+              action={<ItemButton mediaQueries={mediaQueries} />}
+              image={oranges}
+              price={20}
+              stars={4}
+              title="Soure Fresh Lemons"
+              mediaQueries={mediaQueries}
+            />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
