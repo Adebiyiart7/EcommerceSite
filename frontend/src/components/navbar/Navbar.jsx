0be50feb8a680e19dc34b56b84af9e1e8d5b6e54
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-11-07 19:21:03 +0100

    Updated navbar to add show/hide functionality to wishlist button

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { makeStyles } from "@mui/styles";
 import { BiHeart } from "react-icons/bi";
 import { MdMenu } from "react-icons/md";
 import { useTheme, useMediaQuery } from "@material-ui/core";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 // LOCAL IMPORTS
 import logo from "../../assets/images/logo.png";
@@ -16,6 +16,10 @@ import Category from "../category";
 import Navigations from "./Navigations";
 import MenuList from "./MenuList";
 import SelectedProducts from "../common/SelectedProducts";
+import {
+  removeFromWishlist,
+  reset,
+} from "../../features/wishlist/wishlistSlice";
 
 const useStyles = makeStyles({
   center: {
@@ -69,8 +73,10 @@ const useStyles = makeStyles({
 const Navbar = ({ mediaQueries, color }) => {
   const theme = useTheme();
   const classes = useStyles();
+  const dispatch = useDispatch();
   const [showMobileNavs, setShowMobileNavs] = useState(false);
   const [showUserMenuList, setShowUserMenuList] = useState(false);
+  const [showWishlist, setShowWishlist] = useState(false);
   const media1000Down = useMediaQuery(theme.breakpoints.down(1000));
   const media650Down = useMediaQuery(theme.breakpoints.down(650));
   const { mediumDown, largeDown } = mediaQueries;
@@ -79,7 +85,7 @@ const Navbar = ({ mediaQueries, color }) => {
 
   const { wishlist } = useSelector((state) => state.wishlist);
 
-  const mediaStyles = {
+  const inlineStyles = {
     avatar: {
       cursor: "pointer",
     },
@@ -100,19 +106,46 @@ const Navbar = ({ mediaQueries, color }) => {
       color: color ? color : "var(--primaryText)",
       padding: largeDown && "7px 0",
     },
+    wishlistContainer: {
+      zIndex: "var(--zIndex2)",
+      color: "var(--secondaryText)",
+      fontFamily: "'Roboto', serif",
+      position: "fixed",
+      top: 0,
+      left: showWishlist ? 0 : -350,
+      transition: ".3s",
+      width: "100%",
+      maxWidth: 320,
+    },
   };
+
+  // remove item from wishlist
+  const handleRemove = (id) => {
+    dispatch(removeFromWishlist(id));
+    dispatch(reset());
+  };
+
+  // show or hide wishlist
+  const handleShowWishlist = () => setShowWishlist(!showWishlist);
 
   return (
     <Container>
-      <SelectedProducts header={"Wishlist"}  items={wishlist} />
-      <div className={classes.navbar} style={mediaStyles.navbar}>
+      <div style={inlineStyles.wishlistContainer}>
+        <SelectedProducts
+          header={"Wishlist"}
+          items={wishlist}
+          onRemove={(id) => handleRemove(id)}
+          onClose={handleShowWishlist}
+        />
+      </div>
+      <div className={classes.navbar} style={inlineStyles.navbar}>
         <a href="/" className={classes.logoContainer}>
           {mediumDown ? (
             <img className={classes.logo} src={logo_sm} alt="logo" />
           ) : (
             <img
               className={classes.logo}
-              style={mediaStyles.logo}
+              style={inlineStyles.logo}
               src={logo}
               alt="logo"
             />
@@ -126,10 +159,10 @@ const Navbar = ({ mediaQueries, color }) => {
         </div>
         <div className={classes.right}>
           <IoSearch className={classes.rightIcon} />
-          <BiHeart className={classes.rightIcon} />
+          <BiHeart onClick={handleShowWishlist} className={classes.rightIcon} />
           <MdMenu
             className={classes.rightIcon}
-            style={mediaStyles.menuBar}
+            style={inlineStyles.menuBar}
             onClick={handleShowMobileNav}
           />
           {(user || media1000Down) && (
@@ -137,11 +170,11 @@ const Navbar = ({ mediaQueries, color }) => {
               <Avatar
                 variant="rounded"
                 onClick={() => setShowUserMenuList(!showUserMenuList)}
-                style={mediaStyles.avatar}
+                style={inlineStyles.avatar}
                 src=""
                 sx={{ width: 34, height: 34, bgcolor: "var(--primaryColor)" }}
               />
-              <ul className={classes.menuList} style={mediaStyles.menuList}>
+              <ul className={classes.menuList} style={inlineStyles.menuList}>
                 <MenuList user={user} />
               </ul>
             </span>
@@ -149,7 +182,7 @@ const Navbar = ({ mediaQueries, color }) => {
         </div>
       </div>
       {media1000Down && (
-        <div style={mediaStyles.mobileNavs}>
+        <div style={inlineStyles.mobileNavs}>
           <Navigations user={user} media1000Down={media1000Down} />
         </div>
       )}
