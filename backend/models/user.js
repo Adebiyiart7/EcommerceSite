ab2da6c31e3b21c7c2d2c8d4ddd5fb06fe8c06ae
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-11-14 04:57:17 +0100

    Add generate auth token to user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,22 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema(
   {
-    fullname: {
+    specialID: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    first_name: {
+      min: 3,
+      max: 255,
+      trim: true,
+      type: String,
+      require: [true, "This field must not be empty"],
+    },
+    last_name: {
       min: 3,
       max: 255,
       trim: true,
@@ -22,8 +36,29 @@ const userSchema = mongoose.Schema(
       type: String,
       require: [true, "This field must not be empty"],
     },
+    isSuperAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: { type: Boolean, default: false },
+    isActive: { type: Boolean, default: true }, // change default to false and activate user with an email
   },
   { timestamps: true }
 );
+
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      isActive: this.isActive,
+      isAdmin: this.isAdmin,
+      isSuperAdmin: this.isSuperAdmin,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+};
 
 module.exports = mongoose.model("User", userSchema);
